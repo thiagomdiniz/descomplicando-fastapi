1679a6db24d48fd19cf5bee120987c9e8cf10fcb
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2024-05-27 04:49:54 +0000

    add cors middleware

--- pamps/app.py
+++ pamps/app.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.staticfiles import StaticFiles
+
 from .routes import main_router
+from .config import settings
 
 app = FastAPI(
     title="Pamps",
     version="0.1.0",
     description="Pamps is a posting app",
+    contact={
+        "name": "Contact Name",
+        "url": "http://localhost",
+        "email": "contact@localhost.com",
+    }
 )
 
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 app.include_router(main_router)
+
+#app.mount("/static", StaticFiles(directory="app/static"), name="static")
